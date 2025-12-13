38816aca4c31da826be7cd6daf0afb3476ced756
santhoshverse <bantusanthosh89@gmail.com>
2025-12-13 15:22:23 +0530

    Fix typo in Room model primary key definition

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -2,7 +2,7 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../db.js";
 
 const Room = sequelize.define("Room", {
-    id: { type: DataTypes.BIGINT, autoIncrement: true, primaryPrimary: true },
+    id: { type: DataTypes.BIGINT, autoIncrement: true, primaryKey: true },
     room_id: { type: DataTypes.STRING(64), unique: true },
     name: DataTypes.STRING,
     host_user_id: DataTypes.BIGINT,
