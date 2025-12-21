bcee408785f715324a76de4d371e6a76dd48a9c8
santhoshverse <bantusanthosh89@gmail.com>
2025-12-21 14:09:45 +0530

    cleanup: remove all authentication related code and files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import RoomPage from "./pages/RoomPage";
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "PASTE_YOUR_ID_HERE";
 
 export default function App() {
     return <RoomPage />;
